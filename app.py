ccf91dd61b79c76dde12ceff52db27429c37071e
Shaantam Chawla <shaantam_c@yahoo.com>
2015-07-18 22:31:28 -0400

    improved keywords for each topic

--- app.py
+++ app.py
@@ -9,16 +9,25 @@ app = Flask(__name__)
 
 #obama search = https://www.google.com/webhp?oe=utf8&source=uds&start=0&h1=en&q=barack+obama
 
-key_words = ['tax', 'taxes', 'gay', 'lesbian', 'employment', 'union', 'abortion', 'discrimination', 'affirmative action', 
-	'privacy', 'gambling', 'torture', 'immigration', 'education', 'teachers', 'energy', 'gas', 'nuclear', 'renewable', 
-	'global warning', 'loans', 'student loans', 'welfare', 'social security', 'food stamps', 'disability', 'hate crime', 'police abuse', 
-	'police brutality', 'prison', 'crime', 'execution', 'death penalty', 'death sentence', 'capital punishment', 'gun', 'weapon',
-	'drone', 'combat', 'mass destruction', 'chemical', 'biological', 'science' 'terror', 'war', 'drugs', 'marijuana', 
-	'medical marijuana', 'capitalism', 'economy', 'policy', 'legislation', 'law', 'view', 'security', 'minimum wage', 'god', 
-	'evolution', 'israel', 'iraq', 'afghanistan', 'north korea', 'mexico', 'cuba', 'iran', 'pakistan', 'taliban', 'al-qaeda', 'palestine', 
-	'gaza', 'oil', 'shooting', 'environment', 'healthcare', 'pension', 'poverty', 'middle class', 'lower class', 'upper class', 'college',
-	'school', 'transportation', 'fracking', 'water quality', 'foreign', 'isis', 'insurance', 'carbon', 'greenhouse', 'pandemic',
-	'epidemic', 'housing', 'racism', 'racial', 'race', 'black', 'african american', 'foreign', 'foreign policy', 'foreign relations']
+economic_key_words = ['economy', 'economic' 'tax', 'taxes', 'budget', 'debt', 'employment', 'recession', 'financial']
+health_key_words = ['health care', 'obamacare', 'health reform', 'medicare', 'medicaid', 'health insurance', 'pandemic', 'condom', 'aids']
+energy_key_words = ['energy', 'fracking', 'water quality', 'global warming', 'oil', 'nuclear', 'carbon', 'pollution', 'gas', 'coal', 
+					'solar', 'electricity', 'recycling']
+education_key_words = ['education', 'college', 'student loan', 'high school dropout', 'evolution', 'test score', 'teacher', 'pell']
+crime_key_words = ['prison', 'incarceration' 'execution', 'death penalty', 'death sentence', 'capital punishment', 'drug deal', 'use drug', 
+					'sell drug']
+rights_key_words = ['civil rights', 'gay', 'lesbian', 'racial equality', 'black', 'affirmative action', 'job discrimination',
+					'same-sex marriage', 'homosexuality', 'hate crime', 'racism', 'police brutality', 'police violence']
+family_key_words = ['family', 'family values', 'religion', 'christianity', 'abstinence', 'childcare', 'child support', 'abortion', 
+					'sexual abuse']
+foreign_key_words = ['foreign policy', 'terrorism', 'world war', 'iraq', 'afghanistan', 'north korea', 'israel', 'iran', 'qaeda', 'taliban', 'isis',
+					'foreign trade', 'export goods']
+gun_key_words = ['gun control', 'gun rules', 'gun violence', '2nd amendment', 'second amendment', 'gun bans', 'semi-automatic', 
+					'semi automatic']
+security_key_words = ['homeland security', 'national security', 'nuclear weapons', 'nuclear stockpile', 'terrorist threats', 'privacy',
+						'torture', 'ptsd', 'homeless veteran', 'drone', 'mass destruction']
+immigration_key_words = ['immigration', 'illegal immigrant', 'border patrol', 'path to citizenship', ' legal immigrant', 'foreign student']
+poverty_key_words = ['poverty', 'welfare', 'homelessness', 'inner city', 'minimum wage', 'food stamps', 'lower class']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -91,8 +100,11 @@ def analyze(urls):
 				for key_word in key_words:
 					if key_word in item:
 						temp = item
-						if item[-2:] == "\\r":
+						if temp[-2:] == "\\r":
 							temp = item[:-2]
+
+						if temp[-1:] == ")":
+							temp = temp[:-11]
 
 						facts.append(temp)
 						fact_count += 1
